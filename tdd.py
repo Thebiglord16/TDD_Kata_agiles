aaf7a58169d98b5afd6e608fbd96d3df10494a77
unknown <l.mejia@uniandes.edu.co>
2021-03-21 22:06:36 -0500

    minor bug fix and code readibility: GREEN

--- tdd.py
+++ tdd.py
@@ -1,10 +1,9 @@
-def statsSecuence(numberList):
-    
+def statsSecuence(numberList):  
     respuesta = []
     cantidad = 0
     max = 0
-    min = 0
     sum = 0
+    min = 0
     if not numberList.strip():
         cantidad = 0
         respuesta.append(cantidad)
@@ -12,20 +11,24 @@ def statsSecuence(numberList):
         respuesta.append(max)
         respuesta.append(0)
         return respuesta
-    numberList = numberList.split(" ")
-    i = 0
-    while i < cantidad:
-        numberList[i] = float(numberList[i])
-        if numberList[i] >= max:
-            max = numberList[i]
-        if numberList[i] <= min:
-            min = numberList[i]
-        sum += numberList[i]
-        i += 1
-    respuesta = []
-    respuesta.append(cantidad)
-    respuesta.append(min)
-    respuesta.append(max)
-    respuesta.append(sum/cantidad)
-    return respuesta
+    else:
+        numberList = numberList.split(" ")
+        cantidad = len(numberList)
+        min = float(numberList[0])
+        i = 0
+        while i < cantidad:
+            numberList[i] = float(numberList[i])
+            if numberList[i] >= max:
+                max = numberList[i]
+            if numberList[i] <= min:
+                min = numberList[i]
+            sum += numberList[i]
+            i += 1
+        respuesta = []
+        respuesta.append(cantidad)
+        respuesta.append(min)
+        respuesta.append(max)
+        respuesta.append(sum/cantidad)
+        return respuesta
 
+print(statsSecuence("3 9"))
