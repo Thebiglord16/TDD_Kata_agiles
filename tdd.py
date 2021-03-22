77e04e57350519400aab005ff232fafb975135ec
unknown <l.mejia@uniandes.edu.co>
2021-03-21 21:29:11 -0500

    se construyo la funcion que reorna las estadisticas de la secuencia

---
+++ tdd.py
@@ -0,0 +1,23 @@
+def statsSecuence(numberList):
+    numberList = numberList.split(" ")
+    cantidad = len(numberList)
+    i = 1
+    max = float(numberList[0])
+    min = float(numberList[0])
+    sum = float(numberList[0])
+    while i < cantidad:
+        numberList[i] = float(numberList[i])
+        if numberList[i] > max:
+            max = numberList[i]
+        if numberList[i] < min:
+            min = numberList[i]
+        sum += numberList[i]
+        i += 1
+    respuesta = []
+    respuesta.append(cantidad)
+    respuesta.append(min)
+    respuesta.append(max)
+    respuesta.append(sum/cantidad)
+    return respuesta
+
+print(statsSecuence("1 35 34 1 23 41 51 32"))
